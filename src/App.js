c58844fe92fc56b7f79ee49eb6c6b61b9a4c39d0
Souravsinha96 <souravsinha1604@gmail.com>
2021-06-11 11:43:41 +0530

    CSS changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
       <TodoList />
       <footer className="footer">
         Made with <span style={{ color: "red" }}>❤ </span>by{" "}
-        <strong>Sourav</strong>
+        <a
+          style={{
+            textDecoration: "none",
+            color: "#125d98",
+          }}
+          href="https://github.com/Souravsinha96/todo-redux"
+          target="_blank"
+        >
+          <strong>Sourav</strong>
+        </a>
       </footer>
     </div>
   );
