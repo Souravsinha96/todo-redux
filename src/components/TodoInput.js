4e695f413f5e60f0eedf21d93759d34d18008886
Souravsinha96 <souravsinha1604@gmail.com>
2021-06-13 14:34:20 +0530

    Added welcome note

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -6,13 +6,40 @@ import date from "date-and-time";
 function TodoInput() {
   const dispatch = useDispatch();
   const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
 
   const inputref = useRef();
+  const inputName = useRef();
   const pattern = date.compile("YYYY-MM-DD");
 
+  //localstorage
   useEffect(() => {
+    const temp = localStorage.getItem("name");
+    const temp1 = localStorage.getItem("content");
+    const loadedName = JSON.parse(temp);
+    const pendingContent = JSON.parse(temp1);
+
+    setName(loadedName);
+    setContent(pendingContent);
+  }, []);
+  useEffect(() => {
+    const temp = JSON.stringify(name);
+    localStorage.setItem("name", temp);
+    const temp1 = JSON.stringify(content);
+    localStorage.setItem("content", temp1);
+  }, [name, content]);
+
+  useEffect(() => {
+    if (!null) inputName.current.focus();
+  }, []);
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setContent(name);
+    setName("");
     inputref.current.focus();
-  });
+  };
 
   const handleSubmit = () => {
     dispatch(
@@ -33,8 +60,21 @@ function TodoInput() {
   };
   return (
     <div className="mainContainer">
-      <h1>..ToDo List..</h1>
-
+      <h1>..My To Do..</h1>
+      {!content ? (
+        <form onSubmit={handleForm}>
+          <input
+            className="profile"
+            ref={inputName}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Please Enter your Name"
+          />
+        </form>
+      ) : (
+        <h4 className="welcome_name">What's up, {content}!</h4>
+      )}
       <button className="deleteTodo" onClick={() => dispatch(deleteAllTodo())}>
         Clear Data
       </button>
@@ -46,7 +86,7 @@ function TodoInput() {
         placeholder="Add a todo"
         onKeyDown={handleKeypress}
       />
-      <button disabled={text === ""} onClick={handleSubmit}>
+      <button disabled={text === "" || content === ""} onClick={handleSubmit}>
         ADD
       </button>
     </div>
